d7d702d3afbee5332166597e08e2bc7408c568fc
yishai <yishai5555@gmail.com>
2025-06-12 11:47:19 +0300

    02-CfnOutput

--- py-starter/py_starter/py_starter_stack.py
+++ py-starter/py_starter/py_starter_stack.py
@@ -1,19 +1,41 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    Duration,
+    CfnOutput
 )
 from constructs import Construct
+
 
 class PyStarterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(
+            self,
+            "PyBucket",
+            lifecycle_rules=[
+                s3.LifecycleRule(
+                    expiration=Duration.days(4),
+                )
+            ],
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyStarterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        CfnOutput(
+            self,
+            "PyBucketName",
+            value=bucket.bucket_name,
+            description="The name of the S3 bucket created by PyStarterStack",
+        )
+
+        # print(f"Bucket created with name: {bucket.bucket_name}")
+
+        #                s3.LifecycleRule(
+        #         id="DeleteOldVersions",
+        #         noncurrent_version_expiration=aws_cdk.Duration.days(30)
+        #     )
+        # bucket_name="py-starter-bucket",
+        # versioned=True,
+        # removal_policy=aws_cdk.RemovalPolicy.DESTROY,
+        # auto_delete_objects=True
