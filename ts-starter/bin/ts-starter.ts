66b57b147e33b8acce469f08f7b2fa81a25cf59a
yishai <yishai5555@gmail.com>
2025-06-25 14:07:54 +0300

    07-fixes

--- ts-starter/bin/ts-starter.ts
+++ ts-starter/bin/ts-starter.ts
@@ -3,8 +3,12 @@ import * as cdk from 'aws-cdk-lib';
 import { TsStarterStack } from '../lib/ts-starter-stack';
 
 const app = new cdk.App();
-const starteStack: TsHandlerStackProps = new TsStarterStack(app, 'TsStarterStack')
+const starteStack = new TsStarterStack(app, 'TsStarterStack')
 const bucketname = starteStack.coolBucket.bucketName;
 new TsStarterStack(app, 'TsStarterStack', {
   coolBucket: starteStack.coolBucket,
 });
+
+
+// error:
+// Object literal may only specify known properties, and 'coolBucket' does not exist in type 'StackProps'.ts(2353)
--- ts-starter/lib/ts-handler-stack.ts
+++ ts-starter/lib/ts-handler-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 interface TsHandlerStackProps extends cdk.StackProps {
   coolBucket: Bucket;
@@ -14,10 +14,10 @@ export class TsHandlerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: TsHandlerStackProps) {
     super(scope, id, props);
 
-    new LambdaFunction(this, 'TsCoolLambda', {
+    new Function(this, 'TsCoolLambda', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'index.handler',
-      code = Code.fromInline(`
+      code: Code.fromInline(`
                 exports.handler = async (event) => {
                   console.log('Event Bucket:', process.env.COOL_BUCKET_ARN)
                 };
